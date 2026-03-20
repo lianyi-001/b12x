5d54f4732b726ce9e4b498075776fb6f465ffe06
Luke Alonso <lalonso@gmail.com>
2026-03-20 08:51:40 -0700

    Revert "attn: pack flattened fp8 fragment words"
    This reverts commit b57f42a1fb7cb33ea54920e855b2d3786775755b.

--- b12x/attention/forward.py
+++ b12x/attention/forward.py
@@ -108,11 +108,17 @@ def convert_fp8_fragment_to_bf16(
     src: cute.Tensor,
     transpose: cutlass.Constexpr = False,
 ):
-    src_u32 = cute.recast_tensor(cute.flatten(cute.recast_tensor(src, cutlass.Uint8)), cutlass.Uint32)
+    src_u8 = cute.flatten(cute.recast_tensor(src, cutlass.Uint8))
     dst_u32 = cute.recast_tensor(dst, cutlass.Uint32)
     num_packed = cute.size(dst_u32.shape) // 2
     for i in cutlass.range_constexpr(num_packed):
-        bf2_lo, bf2_hi = fp8x4_e4m3_to_bfloat2x2(src_u32[i])
+        packed = (
+            cutlass.Uint32(src_u8[4 * i + 0])
+            | (cutlass.Uint32(src_u8[4 * i + 1]) << cutlass.Uint32(8))
+            | (cutlass.Uint32(src_u8[4 * i + 2]) << cutlass.Uint32(16))
+            | (cutlass.Uint32(src_u8[4 * i + 3]) << cutlass.Uint32(24))
+        )
+        bf2_lo, bf2_hi = fp8x4_e4m3_to_bfloat2x2(packed)
         dst_u32[2 * i + 0] = bf2_lo
         dst_u32[2 * i + 1] = bf2_hi
 
@@ -121,10 +127,8 @@ def convert_fp8_fragment_to_bf16(
 def copy_flattened(src: cute.Tensor, dst: cute.Tensor):
     src_flat = cute.flatten(src)
     dst_flat = cute.flatten(dst)
-    src_u32 = cute.recast_tensor(src_flat, cutlass.Uint32)
-    dst_u32 = cute.recast_tensor(dst_flat, cutlass.Uint32)
-    for i in cutlass.range_constexpr(cute.size(dst_u32.shape)):
-        dst_u32[i] = src_u32[i]
+    for i in cutlass.range_constexpr(cute.size(dst_flat.shape)):
+        dst_flat[i] = src_flat[i]
 
 
 @cute.jit
